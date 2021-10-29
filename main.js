7473f79cb3af536a35bbf9d9cf546d5751a6a9dd
AarnaMankad <81700625+AarnaMankad@users.noreply.github.com>
2021-10-29 20:34:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+prediction1= "";
+prediction2="";
+
+Webcam.set({
+width:350,
+height:300,
+image_format: 'png',
+png_quality:90
+});
+
+camera=document.getElementById("camera")
+Webcam.attach('#camera');
+
+
+function take_snapshot()
+{
+Webcam.snap(function(data_uri) {
+document.getElementById("result").innerHTML ='<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+
+console.log("ml5 version",ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yoTKsOody/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("ModelLoaded");
+}
+
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data_1="The First Prediction Is "+prediction1;
+    speak_data_2="And The Prediction Is "+prediction2;
+
+    var utterThis= new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+
+}
